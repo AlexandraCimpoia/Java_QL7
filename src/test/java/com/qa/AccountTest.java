959eed839ab38e2a3c941f260a957d24e532f7cf
AlexandraCimpoia <alexandra.cimpoia13@gmail.com>
2023-07-27 09:22:52 +0100

    Added "Check Account Number" test

--- src/test/java/com/qa/AccountTest.java
+++ src/test/java/com/qa/AccountTest.java
@@ -9,17 +9,26 @@ import static org.junit.jupiter.api.Assertions.*;
 class AccountTest {
 
     private Account act;
+
     @BeforeEach
     void setUp() {
-        act = new Account(new String[]{"Link","Visa"},
-                "10-10-22","11223344","1234");
+        act = new Account(new String[]{"Link", "Visa"},
+                "10-10-22", "11223344", "1234");
     }
 
     @Test
     @DisplayName("Match Accounts")
     void compareTo() {
         Account inputObject = new Account(new String[]{"Link"},
-                "10-10-22","11223344","1234");
+                "10-10-22", "11223344", "1234");
         assertEquals(0, act.compareTo(inputObject), "Login Failed");
     }
+
+    @Test
+    @DisplayName("Check Account Number")
+    void getAccountNumber() {
+        assertEquals("11223344", act.getAccountNumber(),
+                "Account Number mismatch");
+    }
+
 }
